0b343c18f15761b48dfbdb29dd92236d985c1bc4
broodroosterdev <myth.usa538@gmail.com>
2020-09-11 16:16:41 +0200

    2:2 Implementing Classes

--- Model/Driver.cs
+++ Model/Driver.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace Model
 {
-    class Driver
+    class Driver : IParticipant
     {
+        private string _name;
+        public string Name { get => _name; set => _name = value; }
+        private int _points;
+        public int Points { get => _points; set => _points = value; }
+        private IEquipment _equipment;
+        public IEquipment Equipment { get => _equipment; set => _equipment = value; }
+        private TeamColors _teamcolor;
+        public TeamColors TeamColor { get => _teamcolor; set => _teamcolor = value; }
     }
 }
