6638ce4a6b47f96b86f2f1b625db5c824f76074e
broodroosterdev <myth.usa538@gmail.com>
2020-09-14 09:01:26 +0200

    3:1 Testing your code

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    class Model_Competition_NextTrackShould
+    {
+        private Competition _competition;
+
+        [SetUp]
+        public void Setup()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+    }
+}
--- Model/Competition.cs
+++ Model/Competition.cs
@@ -6,8 +6,8 @@ namespace Model
 {
     public class Competition
     {
-        public List<IParticipant> Participants { get; set; }
-        public Queue<Track> Tracks { get; set; }
+        public List<IParticipant> Participants { get; set; } = new List<IParticipant>();
+        public Queue<Track> Tracks { get; set; } = new Queue<Track>();
         public Track NextTrack()
         {
             if (Tracks.Count > 0)
