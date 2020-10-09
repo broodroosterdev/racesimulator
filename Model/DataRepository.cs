0504020190a2c52d0d15ef52ae250026f479769d
broodroosterdev <myth.usa538@gmail.com>
2020-10-09 16:41:47 +0200

    6:1 Smart coding

---
+++ Model/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class DataRepository<T>
+    {
+        private List<T> _list = new List<T>();
+
+        public void AddValue(T value)
+        {
+            _list.Add(value);
+        }
+    }
+}
