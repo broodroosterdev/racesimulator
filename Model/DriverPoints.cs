0884511593fe0a8fd237e124b7b9b93d9f8fd106
broodroosterdev <myth.usa538@gmail.com>
2020-10-09 17:05:30 +0200

    6:2 Data templates

---
+++ Model/DriverPoints.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class DriverPoints
+    {
+        public String Name { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ Model/LapTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class LapTime
+    {
+        public String Name { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
