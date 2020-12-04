9f173a01f5cbd03d179c330a725ebf54cb06d922
broodroosterdev <myth.usa538@gmail.com>
2020-12-04 14:27:55 +0100

    7:3 Loading and Caching

---
+++ RaceSimulatorGUI/ImageCache.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace RaceSimulatorGUI
+{
+    public class ImageCache
+    {
+        private Dictionary<string, Bitmap> _cache = new Dictionary<string, Bitmap>();
+
+        public Bitmap GetImage(string key)
+        {
+            Bitmap bitmap;
+            if (_cache.TryGetValue(key, out bitmap))
+            {
+                return bitmap;
+            }
+            else
+            {
+                bitmap = new Bitmap(key);
+                _cache.Add(key, bitmap);
+                return bitmap;
+            }
+        }
+
+        public void Clear() => _cache.Clear();
+    }
+}
