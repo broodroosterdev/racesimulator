e712ec2ebb32c6a2bf6a20427f92b4aeef50267d
broodroosterdev <myth.usa538@gmail.com>
2020-12-28 15:36:36 +0100

    8.3 Data Context

---
+++ RaceSimulatorGUI/MainWindowData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RaceSimulatorGUI.Annotations;
+
+namespace RaceSimulatorGUI
+{
+    public class MainWindowData : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
